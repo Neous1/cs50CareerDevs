56aab7bc5026a846a59ab247287f782eceb4cf3b
Neous1 <Neous1@users.noreply.github.com>
2017-11-01 16:23:43 +0000

    add fibonacci

--- javaScript2C_classathon/fibonacci/sumFibs.c
+++ javaScript2C_classathon/fibonacci/sumFibs.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int sum = 0, curFib = 1, priorFib = 0, num = 0;
+int sumFibs (int num);
 
 int main(void)
 {
-    printf("get me a number\");
-    sum = get_int();
+    // int curFib = 1, priorFib = 0, 
+    int num = 0;
+        // int num = 0;
+    printf("number please: ");
+    num = get_int();
+    int result = sumFibs(num);
+    printf(" 14. %d\n", result);
+    
+}
+
+int sumFibs (int num){
+    int sum = 0,  curFib = 1, priorFib = 0;
+    // printf("get me a number\n");
+    // sum = get_int();
     while (curFib <= num)
-    {
+    {        
+        printf(" 24. %d\n", num);
+        printf(" 25. %d\n", curFib);
         if(curFib % 2 != 0)
         {
             sum += curFib; 
         }
+        
+        int temp = priorFib;
+        priorFib = curFib;
+        curFib += temp;
     }
     return sum ;
 }
