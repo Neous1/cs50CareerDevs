a19f33923d160cb8af3e7aea878fdedf1f033a83
Neous1 <Neous1@users.noreply.github.com>
2017-11-02 17:59:21 +0000

    refactored capitalize1

--- week2/capitalize1.c
+++ week2/capitalize1.c
@@ -11,15 +11,8 @@ int main(void)
     {
         for (int i = 0, n = strlen(s); i < n; i++ )
         {
-            if(s[i] >= 'a' && s[i] <= 'z')
-            {
                 printf("%c", toupper(s[i]));
-            }
-            else
-            {
-                
-                printf("%c", s[i]);
-            }
+
         }
         printf("\n");
     }
