eb5b0d727ff78555e42fe31e9eb49650ab38dfb5
Neous1 <Neous1@users.noreply.github.com>
2017-11-03 13:55:35 +0000

    reorganized in class coding files

--- javaScript2C_classathon/sumAllPrimes/sumAllPrime2.c
+++ javaScript2C_classathon/sumAllPrimes/sumAllPrime2.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+//check50 cs50/2017/x/initials/less
+
 
 bool isPrime(int n); //is Prime = memoryAddress
 
 int main(void)
 {
     printf("%d\n", isPrime(10));
-    printf("is Prime (2) %d\n", isPrime(13));
+    printf("is Prime (13) %d\n", isPrime(13));
 }
 
 bool isPrime(int n)
@@ -25,9 +27,11 @@ bool isPrime(int n)
 int sumPrimes(int num)
 {
     int sum = 0; 
-    for (int i = num ; i>=2; i){
+    for (int i = num ; i >= 2; i--){
         if(isPrime(i)){
             sum += i;
         }
     }
+    
+    return sum ;
 }
